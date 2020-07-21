5e5b1f4b0dc53b0c3b3903bb05f9e1f72fa922ad
Dima <ddligach@luc.edu>
2020-07-21 13:00:13 -0500

    modifed the old version of dataset from Universal/ to use my own tokenizer

---
+++ Codes/dataset.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+
+import configparser, os, pandas, sys, pathlib, shutil, pickle
+import tokenizer
+
+model_dir = 'Model/'
+
+alphabet_file = 'Model/alphabet.txt'
+alphabet_pickle = 'Model/alphabet.p'
+
+diag_icd_file = 'DIAGNOSES_ICD.csv'
+proc_icd_file = 'PROCEDURES_ICD.csv'
+cpt_code_file = 'CPTEVENTS.csv'
+
+class DatasetProvider:
+  """Notes and ICD code data"""
+
+  def __init__(self,
+               input_dir,
+               output_dir,
+               max_cuis,
+               max_codes):
+    """Construct it"""
+
+    self.input_dir = input_dir
+    self.output_dir = output_dir
+
+    if os.path.isdir(model_dir):
+      shutil.rmtree(model_dir)
+    os.mkdir(model_dir)
+
+    # encounter ids -> icd code sets
+    self.enc2codes = {}
+
+    diag_code_path = os.path.join(self.output_dir, diag_icd_file)
+    proc_code_path = os.path.join(self.output_dir, proc_icd_file)
+    cpt_code_path = os.path.join(self.output_dir, cpt_code_file)
+
+    self.index_codes(diag_code_path, 'ICD9_CODE', 'diag', 3)
+    self.index_codes(proc_code_path, 'ICD9_CODE', 'proc', 2)
+    self.index_codes(cpt_code_path, 'CPT_NUMBER', 'cpt', 5)
+
+    # index inputs (cuis)
+    self.input_tokenizer = tokenizer.Tokenizer(
+      n_words=None if max_cuis == 'all' else int(max_cuis),
+      lower=False,
+      oov_token='oovtok')
+    self.tokenize_input()
+
+    # index outputs (codes)
+    self.output_tokenizer = tokenizer.Tokenizer(
+      n_words=None if max_codes == 'all' else int(max_codes),
+      lower=False,
+      oov_token='oovtok')
+    self.tokenize_output()
+
+  def index_codes(self, code_file, code_col, prefix, num_digits):
+    """Map encounters to codes"""
+
+    frame = pandas.read_csv(code_file, dtype='str')
+
+    for id, code in zip(frame['HADM_ID'], frame[code_col]):
+      if pandas.isnull(id):
+        continue # some subjects skipped (e.g. 13567)
+      if pandas.isnull(code):
+        continue
+      if id not in self.enc2codes:
+        self.enc2codes[id] = set()
+
+      short_code = '%s_%s' % (prefix, code[0:num_digits])
+      self.enc2codes[id].add(short_code)
+
+  def tokenize_input(self):
+    """Read text and map tokens to ints"""
+
+    x = [] # input documents
+    for file_path in pathlib.Path(self.input_dir).glob('*.txt'):
+      x.append(file_path.read_text())
+
+    self.input_tokenizer.fit_on_texts(x)
+    pickle_file = open('Model/tokenizer.p', 'wb')
+    pickle.dump(self.input_tokenizer, pickle_file)
+    print('input vocab:', len(self.input_tokenizer.stoi))
+
+  def tokenize_output(self):
+    """Map codes to ints"""
+
+    y = [] # prediction targets
+    for _, codes in self.enc2codes.items():
+      y.append(' '.join(codes))
+
+    self.output_tokenizer.fit_on_texts(y)
+    print('output vocab:', len(self.output_tokenizer.stoi))
+
+  def load(self):
+    """Make x and y"""
+
+    x = []
+    y = []
+
+    # make a list of inputs and outputs to vectorize
+    for file_path in pathlib.Path(self.input_dir).glob('*.txt'):
+      if file_path.stem not in self.enc2codes:
+        continue
+
+      x.append(file_path.read_text())
+      codes_as_string = ' '.join(self.enc2codes[file_path.stem])
+      y.append(codes_as_string)
+
+    # make x and y matrices
+    x = self.input_tokenizer.texts_to_seqs(x)
+    y = self.output_tokenizer.texts_to_seqs(y)
+
+    # column zero is empty
+    # return x, y[:,1:]
+    return x, y
+
+if __name__ == "__main__":
+  """Test dataset class"""
+
+  cfg = configparser.ConfigParser()
+  cfg.read(sys.argv[1])
+  base = os.environ['DATA_ROOT']
+
+  dp = DatasetProvider(
+    os.path.join(base, cfg.get('data', 'cuis')),
+    os.path.join(base, cfg.get('data', 'codes')),
+    cfg.get('args', 'max_cuis'),
+    cfg.get('args', 'max_codes'))
+
+  inputs, outputs = dp.load()
+  print(inputs[:2])
+  print(outputs[:2])
---
+++ Codes/tokenizer.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import collections
+
+class Tokenizer:
+  """Tokenization and vectorization"""
+
+  def __init__(self, n_words, lower=False, oov_token='oovtok'):
+    """Construction deconstruction"""
+
+    self.stoi = {}
+    self.itos = {}
+
+    self.n_words = None if n_words is None else n_words-1
+    self.lower = lower
+    self.oov = oov_token
+
+    self.stoi[oov_token] = 0
+    self.itos[0] = oov_token
+
+  def fit_on_texts(self, texts):
+    """Fit on a list of document strings"""
+
+    counts = collections.Counter()
+
+    for text in texts:
+      tokens = text.split()
+      counts.update(tokens)
+
+    index = 1
+    for token, _ in counts.most_common(self.n_words):
+      self.stoi[token] = index
+      self.itos[index] = token
+      index += 1
+
+  def texts_to_seqs(self, texts):
+    """List of strings to list of int sequences"""
+
+    sequences = []
+    for text in texts:
+
+      sequence = []
+      for token in text.split():
+        if token in self.stoi:
+          sequence.append(self.stoi[token])
+        else:
+          sequence.append(self.stoi[self.oov])
+
+      sequences.append(sequence)
+
+    return sequences
+
+  def texts_as_sets_to_seqs(self, texts):
+    """Same as texts_to_sequences but treat texts as sets"""
+
+    sequences = []
+    for text in texts:
+
+      sequence = []
+      for token in set(text.split()):
+        if token in self.stoi:
+          sequence.append(self.stoi[token])
+        else:
+          sequence.append(self.stoi[self.oov])
+
+      sequences.append(sequence)
+
+    return sequences
+
+if __name__ == "__main__":
+
+  sents = ['it is happening again',
+           'the owls are not what they seem',
+           'again and again',
+           'the owls are happening']
+
+  tokenizer = Tokenizer(n_words=5)
+
+  tokenizer.fit_on_texts(sents)
+  print(tokenizer.stoi)
+  print(tokenizer.itos)
+
+  seqs = tokenizer.texts_to_sequences(sents)
+  print(seqs)
